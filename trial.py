201c004e35851fc217ca4abdb9f0a8efdf82bf43
yoshisam1 <samuel171070@gmail.com>
2025-01-17 00:33:18 +0700

    Initial commit for serper_test branch

--- trial.py
+++ trial.py
@@ -1,1 +1,8 @@
-print ("hello world")
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from langchain.tools import Tool
+from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, List, Dict, Tuple
+import os
